c008bd1c0cfa83488ff0d3e889574e1fc5bee285
İbrahim Yazıcı <ibrahim.yazici@habermetre.com>
2018-06-22 11:08:03 +0300

    dfrg

--- testr/Form1.cs
+++ testr/Form1.cs
@@ -20,6 +20,7 @@ namespace testr
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("asdasdasdasd");
+            MessageBox.Show("editted");
         }
     }
 }
